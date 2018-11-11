4893a3bb5a2a98d0d53dac696fb3a344bc431eba
eyob tariku <eyobtariku@gmail.com>
2018-11-11 12:58:54 +0300

    [#4] - Move global database setting to `config.settings.local`

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,21 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
--- config/settings/local.py
+++ config/settings/local.py
@@ -8,3 +8,17 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 INSTALLED_APPS += [
     'django_extensions',
 ]
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
+    }
+}
