c49d4aaf467c42ca935254b23ea7356435fe43de
eyob tariku <eyobtariku@gmail.com>
2018-11-11 13:09:08 +0300

    [#4] - Add database credentials in `config.settings.testing` module

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -3,3 +3,18 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travis_ci_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
