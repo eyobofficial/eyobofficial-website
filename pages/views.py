22cc7cf160548b1016912831eb52a1786554ad4f
eyob tariku <eyobtariku@gmail.com>
2018-11-15 11:35:52 +0300

    [#1] - Modify `pages/base.html` template as the project's base template

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['page_title'] = 'Home'
+        return kwargs
